e26710975915d7110eae51e540533ee192f0bea4
Jéssica Priscila <jesik1611@gmail.com>
2025-08-13 18:22:36 -0300

    Add files via upload
    Aula 13/08

--- Aula1/Atividade5.py
+++ Aula1/Atividade5.py
@@ -1,4 +1,23 @@
-x = "Qual o seu nome?"
-y = input(x)
-z = f"olá, {y}! =)"
-print(z)
+pergunta = "Qual o seu nome?"
+
+resposta = input(pergunta)
+
+
+mensagem_saida = f"olá, {resposta}! =)"
+
+print(resposta)
+
+if resposta =="":
+    resposta = "pessoa"
+
+
+#virgula separa as coisas
+#nos nomes não colocar espaço, use underline
+#Variáveis são espaço de memória que guardam informações 
+#Porque coloca = duas vezes? pois é uma atribuição e não uma comparação
+#if é uma condição,se for verdade executa o que está dentro do if
+# A #serve para fazer comentarios de linha, 3 aspas simples serve para fazer comentários de bloco
+#string é uma cadeira de caracteres, pode ser letras, numeros, simbolos, etc.
+#int é um número inteiro, float é um número decimal
+#input é uma função que lê o que o usuário digita no console
+
