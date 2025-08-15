81fa9e248fa5bff8c9999086f50299f618f7830e
Jéssica Priscila <jesik1611@gmail.com>
2025-08-15 19:50:40 -0300

    Add files via upload
    Prova aula 1

---
+++ Prova_Algoritmos_e_Variáveis.py
@@ -0,0 +1,21 @@
+# Crie um exemplo de cada uma das principais tipagens de variáveis.
+
+inteiro = 10                   # int
+decimal = 3.14                 # float
+texto = "Olá, mundo!"          # str
+booleano = True                # bool
+
+# Mostrando as variáveis
+print("Exemplo de int:", inteiro)
+print("Exemplo de float:", decimal)
+print("Exemplo de string:", texto)
+print("Exemplo de boolean:", booleano)
+
+# Solicitação de dados ao usuário
+nome = input("Digite seu nome: ")
+
+# Mensagem personalizada
+print(f"Seja bem-vindo(a), {nome}! É um prazer ter você aqui!")
+
+
+
